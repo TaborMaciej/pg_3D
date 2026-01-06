7206fed45e35c911da2ecab6f9812e98e5260096
Maciej Tabor <b.maciejtabor@gmail.com>
2026-01-06 19:07:34 +0100

    assignment implementation

---
+++ src/Assignments/MeshesMaterials/app.h
@@ -0,0 +1,67 @@
+//
+// Created by pbialas on 05.08.2020.
+//
+
+#pragma once
+
+#include <vector>
+
+#include "Application/application.h"
+#include "Application/utils.h"
+
+#include "glad/gl.h"
+#include <glm/glm.hpp>
+#include "Camera.h"
+#include "CameraControler.h"
+#include "Engine/Mesh.h"
+#include <Engine/Material.h>
+
+class SimpleShapeApplication : public xe::Application
+{
+public:
+    SimpleShapeApplication(int width, int height, std::string title, bool debug) : Application(width, height, title, debug) {}
+
+    void init() override;
+
+    void frame() override;
+
+    void mouse_button_callback(int button, int action, int mods) override;
+
+    void cursor_position_callback(double x, double y) override;
+
+    void framebuffer_resize_callback(int w, int h) override;
+
+    void scroll_callback(double xoffset, double yoffset) override;
+
+    void set_camera(Camera* camera) { camera_ = camera; }
+
+    void set_controler(CameraControler* controler) { controler_ = controler; }
+    
+    void add_submesh(xe::Mesh* mesh) {
+        meshes_.push_back(mesh);
+    }
+
+    Camera* camera() { return camera_; }
+    CameraControler* controler() { return controler_; }
+    std::vector<xe::Mesh*> meshes() { return meshes_; }
+
+    ~SimpleShapeApplication() {
+        if (camera_) {
+            delete camera_;
+        }
+    }
+
+private:
+    Camera* camera_;
+    CameraControler* controler_;
+    std::vector<xe::Mesh*> meshes_;
+
+    GLuint u_pvm_buffer_handle;
+    float angle_ = 0.0f;
+
+    glm::vec3 axis_;
+    glm::vec3 scale_;
+    glm::vec3 translate_;
+
+    glm::mat4 M_;
+};
