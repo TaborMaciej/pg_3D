6ac4035e818704b30751e77a6d75e52d5f8a4803
Maciej Tabor <b.maciejtabor@gmail.com>
2026-01-13 19:59:43 +0100

    assignment implementation

--- src/Engine/Material.h
+++ src/Engine/Material.h
@@ -11,31 +11,51 @@
 
 namespace xe {
 
-    class Material {
-    public:
-        virtual void bind() = 0;
+	class Material {
+	public:
+		virtual void bind() = 0;
 
-        virtual void unbind() {};
-    };
+		virtual void unbind() {};
+	};
 
-    class ColorMaterial : public Material {
-    public:
-        ColorMaterial(const glm::vec4 color) : color_(color) {}
+	class ColorMaterial : public Material {
+	public:
 
-        void bind();
+		static void init();
 
-        static void init();
+		static GLuint program() { return shader_; }
 
-        static GLuint program() { return shader_; }
+		ColorMaterial(const glm::vec4 color, GLuint texture, GLuint texture_unit) : Kd_(color), texture_(texture),
+			texture_unit_(texture_unit) {}
 
-    private:
-        static GLuint shader_;
-        static GLuint color_uniform_buffer_;
+		ColorMaterial(const glm::vec4 color, GLuint texture) : ColorMaterial(color, texture, 0) {}
 
-        glm::vec4 color_;
+		ColorMaterial(const glm::vec4 color) : ColorMaterial(color, 0) {}
 
-    };
+		void set_texture(GLuint tex) { texture_ = tex; }
+		GLuint get_texture(GLuint tex) { return texture_; }
 
+		void set_texture_unit(GLuint tex_unit) { texture_unit_ = tex_unit; }
+		GLuint get_texture_unit(GLuint tex_unit) { return texture_unit_; }
+
+		void bind() override;
+
+		void unbind() override;
+
+
+
+	private:
+
+		static GLuint shader_;
+		static GLuint color_uniform_buffer_;
+		static GLint uniform_map_Kd_location_;
+
+		glm::vec4 Kd_;
+		GLuint texture_;
+		GLuint texture_unit_;
+
+	};
+	GLuint create_texture(const std::string& name);
 
 }
 
